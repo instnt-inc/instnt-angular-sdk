c4186b1204e8f772b21008c90b36229a18e8bbba
Claudio Teles <telesapps85@gmail.com>
2022-03-09 10:19:14 -0500

    Submitting form after server error

--- src/app/submit-form/submit-form.component.ts
+++ src/app/submit-form/submit-form.component.ts
@@ -14,21 +14,25 @@ export class SubmitFormComponent implements OnInit, OnDestroy {
   errorMessage = '';
   response?: DecisionResponseModel;
   isLoading = false;
+  isServerError = false;
   instnt?: Instnt;
   isSubmited = false;
   constructor(private instntService: InstntAngularService, public data: DataService, private events: EventHandlerService) {
     this.instntService.getInstnt().subscribe((instnt) => {
       this.instnt = instnt;
       console.log('instnt', instnt);
-      
+
     });
   }
-  
+
   ngOnInit(): void {
+    this.setObservable();
+  }
+
+  setObservable() {
     lastValueFrom(this.events.SubmitResult).then((res) => {
-      console.log('first ValueFrom called')
+      this.isServerError = false;
       this.isLoading = false;
-      console.log('Transaction proccessed Response', res);
       this.response = res.data;
     }).catch((err) => {
       console.error('Error Processing Transactions', err);
@@ -37,6 +41,7 @@ export class SubmitFormComponent implements OnInit, OnDestroy {
         this.errorMessage = err;
       } else {
         this.errorMessage = 'There was an error while processing your transaction, please try again'
+        this.isServerError = true;
       }
       this.isSubmited = false;
     });
@@ -46,7 +51,14 @@ export class SubmitFormComponent implements OnInit, OnDestroy {
     this.errorMessage = '';
     this.isLoading = true;
     this.isSubmited = true;
-    this.instnt?.submitData(this.data.userData, false);
+    if (this.isServerError) {
+      this.events.SubmitResult.unsubscribe();
+      this.events.SubmitResult = new ReplaySubject(1);
+      this.setObservable();
+      this.instnt?.submitData(this.data.userData, false);
+    } else {
+      this.instnt?.submitData(this.data.userData, false);
+    }
 
   }
 
