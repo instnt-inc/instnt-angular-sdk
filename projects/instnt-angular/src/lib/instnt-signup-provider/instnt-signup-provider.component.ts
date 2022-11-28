9266930d182257be274219deb5aef4ccd24978d0
Claudio Teles <telesapps85@gmail.com>
2022-11-28 14:00:08 -0500

    Updated SDK to include SDK_Version

--- projects/instnt-angular/src/lib/instnt-signup-provider/instnt-signup-provider.component.ts
+++ projects/instnt-angular/src/lib/instnt-signup-provider/instnt-signup-provider.component.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
-import { Instnt } from '../interfaces/instnt.interface';
+import { SDK_VERSION } from '../version';
+import { ID_METRICS_VERSION } from '../version';
 import { InstntAngularService } from '../instnt-angular.service'
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'instnt-signup-provider',
@@ -14,21 +14,21 @@ import { Observable } from 'rxjs';
 })
 export class InstntSignupProviderComponent implements OnInit, AfterViewInit {
 
-  @Input() formId: string = '';
+  @Input() formKey: string = '';
   @Input() serviceURL: string = '';
   @Input() onEvent: any;
   @Input() children?: any;
-  @Input() hideFormFields?: boolean = true;
-  @Input() redirect?: boolean = false;
+  // @Input() hideFormFields?: boolean = true;
+  // @Input() redirect?: boolean = false;
   @Input() isAsync?: boolean = false;
-  @Input() idmetrics_version? = '4.5.12'
+  @Input() idmetrics_version?= ID_METRICS_VERSION;
   @Input() instntId: string = '';
   @ViewChild('innerDivElement') innerDivElement: ElementRef = new ElementRef(null);
 
   constructor(private http: HttpClient, private service: InstntAngularService) { }
 
   ngOnInit(): void {
-    
+
   }
 
   ngAfterViewInit() {
@@ -41,27 +41,35 @@ export class InstntSignupProviderComponent implements OnInit, AfterViewInit {
     const jQueryFragment = document.createRange().createContextualFragment('<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>');
     this.innerDivElement.nativeElement.appendChild(jQueryFragment);
     let payload: any = {
-      form_key: this.formId,
-      hide_form_fields: this.hideFormFields,
-      redirect: this.redirect,
+      form_key: this.formKey,
+      hide_form_fields: true,
+      redirect: false,
     }
-    if(this.instntId) {
+    if (this.instntId) {
       payload['instnttxnid'] = this.instntId;
       payload['authorization_token'] = this.instntId;
     }
     if (this.serviceURL) {
-      this.http.post(this.serviceURL + `/public/transactions?idmetrics_version=${this.idmetrics_version}`, payload).subscribe({
-        next: (res: any) => {
-          const jQueryFragment = res.html;
-          const fragment = document.createRange().createContextualFragment(jQueryFragment);
-          this.innerDivElement.nativeElement.appendChild(fragment);
-          this.service.instnt.next((window as any).instnt)
-        },
-        error: (err) => { 
-          console.error(err);
-          this.service.instnt.error(err);
-        }
-      });
+      this.http.post(this.serviceURL + `/public/transactions?idmetrics_version=${this.idmetrics_version}&sdk=angular&sdk_version=${SDK_VERSION}`,
+        payload).subscribe({
+          next: (res: any) => {
+            const jQueryFragment = res.html;
+            const fragment = document.createRange().createContextualFragment(jQueryFragment);
+            this.innerDivElement.nativeElement.appendChild(fragment);
+            if ((window as any).instnt) {
+              (window as any).instnt.debug = {
+                sdk: "angular",
+                sdk_version: SDK_VERSION,
+                idmetrics_version: this.idmetrics_version,
+              }
+            }
+            this.service.instnt.next((window as any).instnt)
+          },
+          error: (err) => {
+            console.error(err);
+            this.service.instnt.error(err);
+          }
+        });
     }
   }
 
---
+++ projects/instnt-angular/src/lib/version.ts
@@ -0,0 +1,2 @@
+export const SDK_VERSION = "1.1.2-beta.1";
+export const ID_METRICS_VERSION = "4.5.12"
